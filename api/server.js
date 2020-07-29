f010fa77ac822ba7a6b0435f7152e6a802c8fb61
BassamIshaq <BassamIshaq@github.com>
2020-07-29 23:15:25 +0500

    basic data sending/receiving set up

--- api/server.js
+++ api/server.js
@@ -1,9 +1,29 @@
 var express = require('express')
 var app = express()
 
+var cors = require('cors')
+var bodyParser = require('body-parser')
 
-app.get('/', (req, res)=>{
-    res.send('Just for test')
+app.use(cors())
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/GetData', (req, res)=>{
+    console.log('/GetData route called')
+    
+    res.send({
+        people: [
+            {
+                name: 'Mohsin',
+                age: 20
+            },
+            {
+                name: 'Sheheryar',
+                age: 24
+            }
+        ]
+    })
 })
 
 
--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios'
 import './App.css';
 
 function App() {
@@ -15,6 +16,27 @@ function App() {
     ]
   })
 
+  const [person, setPerson] = useState({
+    people: [
+      {
+        name: 'Ali',
+        age: 22
+      }
+    ]
+  })
+
+  const [serverPath, setServerPath] = useState('http://localhost:5000/')
+
+  useEffect(() => {
+    const fetchdata = async () => {
+      await axios.get(serverPath)
+    }
+    const result = fetchdata()
+    result.then((res) => {
+      setData(res.data)
+    })
+  }, [serverPath])
+
   const displayData = () => {
     var people = data.people.map((person) => {
       return <table>
@@ -30,10 +52,28 @@ function App() {
     })
     return people
   }
+
+  const displaySingleData = () => {
+    var people = person.people.map((singleRecord) => {
+      return <table>
+        <tr>
+          <td>
+            {singleRecord.name}
+          </td>
+          <td>
+            {singleRecord.age}
+          </td>
+        </tr>
+      </table>
+    })
+    return people
+  }
+
+
   return (
     <div className="App">
       <div>
-        <button>Display all records</button>
+        <button onClick={() => { setServerPath('http://localhost:5000/GetData') }}>Display all records</button>
         <div>
           <input placeholder='Enter name of person' />
           <button>Display one record</button>
@@ -55,7 +95,21 @@ function App() {
       </div>
       <div>
         <div>
-          {displayData()}
+          {data.people.map((person) => {
+            return <table>
+              <tr>
+                <td>
+                  {person.name}
+                </td>
+                <td>
+                  {person.age}
+                </td>
+              </tr>
+            </table>
+          })}
+        </div>
+        <div>
+          {displaySingleData()}
         </div>
       </div>
 
