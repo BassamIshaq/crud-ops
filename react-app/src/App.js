cbcad13d8b6817301414bf468001554371c4a289
BassamIshaq <BassamIshaq@github.com>
2020-07-29 22:03:33 +0500

    Displaying Test Data

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,27 +1,62 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [data, setData] = useState({
+    people: [
+      {
+        name: 'Ali',
+        age: 22
+      },
+      {
+        name: 'Talha',
+        age: 23
+      }
+    ]
+  })
+
+  const displayData = () => {
+    var people = data.people.map((person) => {
+      return <table>
+        <tr>
+          <td>
+            {person.name}
+          </td>
+          <td>
+            {person.age}
+          </td>
+        </tr>
+      </table>
+    })
+    return people
+  }
   return (
     <div className="App">
-      <button>Display all records</button>
       <div>
-        <input placeholder='Enter name of person'/>
-        <button>Display one record</button>
+        <button>Display all records</button>
+        <div>
+          <input placeholder='Enter name of person' />
+          <button>Display one record</button>
+        </div>
+        <div>
+          <input placeholder='Enter name of person' />
+          <input placeholder='Enter age' />
+          <button>Create Record</button>
+        </div>
+        <div>
+          <input placeholder='Enter name of person' />
+          <button>Delete record</button>
+        </div>
+        <div>
+          <input placeholder='Enter name of person' />
+          <input placeholder='Enter new age' />
+          <button>Update Record</button>
+        </div>
       </div>
       <div>
-        <input placeholder='Enter name of person'/>
-        <input placeholder='Enter age'/>
-        <button>Create Record</button>
-      </div>
-      <div>
-        <input placeholder='Enter name of person'/>
-        <button>Delete record</button>
-      </div>
-      <div>
-        <input placeholder='Enter name of person'/>
-        <input placeholder='Enter new age'/>
-        <button>Update Record</button>
+        <div>
+          {displayData()}
+        </div>
       </div>
 
     </div>
